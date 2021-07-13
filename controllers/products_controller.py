65addfa85e871324f69e27f8c589332db81efea6
Akwasi Atakora-Acheampong <aa.acheampong1@gmail.com>
2021-07-13 11:07:31 +0100

    10th commit

--- controllers/products_controller.py
+++ controllers/products_controller.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask import Blueprint
+from flask.templating import render_template
 from models.product import Product
 import repositories.product_repository as product_repository
 import repositories.supplier_repository as supplier_repository
 
 products_blueprint = Blueprint("products", __name__)
 
-
-
+@products_blueprint.route("/products")
+def products():
+    products = product_repository.select_all
+    return render_template("products/index.html", all_products = products)
 
 
 
