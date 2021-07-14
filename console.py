99cc6665caa2b3db1dfc1b347b0be64764076e47
Akwasi Atakora-Acheampong <aa.acheampong1@gmail.com>
2021-07-14 12:52:36 +0100

    31st commit added more suppliers and products

--- console.py
+++ console.py
@@ -14,12 +14,22 @@ supplier2 = Supplier("Montredo Watches Ltd")
 supplier_repository.save(supplier2)
 supplier3 = Supplier("Baume & Mercier SA")
 supplier_repository.save(supplier3)
+supplier4 = Supplier("Frederique Constant SA")
+supplier_repository.save(supplier4)
+supplier5 = Supplier("Junghans Uhren GmbH")
+supplier_repository.save(supplier5)
 
-product_1 = Product("Rado Coupole Classic", "R228: Black Leather 37mm", 2, 750, 1159, supplier1)
+product_1 = Product("Rado Coupole Classic", "R228: Black Leather 37mm", 6, 750, 1159, supplier1)
 product_repository.save(product_1)
 
-product_2 = Product("Junghans Meister Classic", "027/3000: Green 38mm Limited Edition", 1, 1050, 1500, supplier2)
+product_2 = Product("Junghans Meister Classic", "027/3000: Green 38mm Limited Edition", 3, 1050, 1500, supplier5)
 product_repository.save(product_2)
 
 product_3 = Product("Baume & Mercier Classima", "MOA1: Brown Leather 40mm", 4, 1000, 1795, supplier3)
 product_repository.save(product_3)
+
+product_4 = Product("Audemars Piguet Rare Octagonal", "15062002: Green Enamel 31mm", 1, 7950, 9250, supplier2)
+product_repository.save(product_4)
+
+product_5 = Product("Rolex Datejust", "16233: Steel and Yellow Gold 36mm", 4, 6000, 7195, supplier2)
+product_repository.save(product_5)
